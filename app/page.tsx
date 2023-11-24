481e0c1965e2a759e40d1bbc377c5a57060ab042
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-11-24 14:57:06 +0100

    Add desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -20,13 +20,16 @@ const contentArray = [
 
 function Accordion({ title, content, className='' }: { key: number, title: string, content: string, className?: string }) {
   const [active, setActive] = useState(false)
+  const [hover, setHover] = useState(false)
   return (
     <div
-      className={`flex flex-col border-b border-divider py-[18px] cursor-pointer`}
+      className={`flex flex-col border-b border-divider py-[18px] cursor-pointer lg:py-[19.5px] ${className}`}
       onClick={() => setActive(!active)}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
       <div className={`flex flex-row justify-between`}>
-        <span className={`text-[13px] leading-[13px] ${active ? 'text-primaryblue font-bold' : 'text-textverydark'}`}>
+        <span className={`text-[13px] leading-[13px] ${active ? 'text-primaryblue font-bold' : 'text-textverydark'} lg:text-[14px] lg:leading-[14px] ${hover ? 'lg:transition lg:text-primaryred' : ''}`}>
           {title}
         </span>
         <div className={`flex flex-col justify-center items-center`}>
@@ -48,18 +51,25 @@ function Accordion({ title, content, className='' }: { key: number, title: strin
 
 export default function Home() {
   return (
-    <div className={`flex flex-col min-h-screen bg-gradient-to-b from-bgviolet to-bgblue`}>
-      <div className={`flex justify-center items-center pt-[148px] pb-[86px]`}>
-        <div className={`relative w-[327px] flex flex-col bg-white rounded-3xl px-6 pb-12 pt-[136px] shadow-[0_50px_50px_-20px_rgba(53,18,122,0.5)]`}>
-          <img className={`absolute top-0 left-[50%] transform -translate-x-1/2 translate-y-[2%]`}
-               src='/images/bg-pattern-mobile.svg' width={236} alt='Background pattern'/>
-          <img className={`absolute top-0 left-[50%] transform -translate-x-1/2 -translate-y-[60%]`}
-            src='/images/illustration-woman-online-mobile.svg' width={236} alt='Background illustration' />
-          <h1 className={`text-[32px] leading-[32px] font-bold mb-[18px] text-center`}>
+    <div className={`flex flex-col min-h-screen bg-gradient-to-b from-bgviolet to-bgblue lg:justify-center`}>
+      <div className={`relative flex justify-center items-center pt-[148px] pb-[86px] lg:py-0`}>
+        <img className={`hidden absolute top-0 left-[50%] transform -translate-x-[290.5%] translate-y-[112%] lg:block lg:w-[190px] z-50`}
+          src='/images/illustration-box-desktop.svg' alt='Box illustration' />
+        <div className={`relative w-[327px] flex flex-col bg-white rounded-3xl px-6 pb-12 pt-[136px] shadow-[0_50px_50px_-20px_rgba(53,18,122,0.5)] lg:w-[920px] lg:pt-16 lg:pl-[475px] lg:pr-24 lg:pb-[85px] lg:overflow-hidden`}>
+          <picture className={`w-[236px] absolute top-0 left-[50%] transform -translate-x-1/2 translate-y-[2%] lg:w-[964px] lg:top-0 lg:-left-0 lg:-translate-x-[60%] lg:-translate-y-[30.5%]`}>
+            <source srcSet='/images/bg-pattern-desktop.svg' media='(min-width: 1024px)' />
+            <img src='/images/bg-pattern-mobile.svg' alt='Background pattern' />
+          </picture>
+          <picture className={`w-[236px] absolute top-0 left-[50%] transform -translate-x-1/2 -translate-y-[60%] lg:w-[472px] lg:top-0 lg:left-0 lg:-translate-x-[18%] lg:translate-y-[20%]`}>
+            <source srcSet='/images/illustration-woman-online-desktop.svg' media='(min-width: 1024px)' />
+            <img src='/images/illustration-woman-online-mobile.svg' alt='Background illustration' />
+          </picture>
+          <h1 className={`text-[32px] leading-[32px] font-bold mb-[18px] text-center lg:text-left`}>
             FAQ
           </h1>
           {titleArray.map((title, index) => {
-            return <Accordion key={index} title={title} content={contentArray[index]} />
+            return <Accordion className={`z-50`}
+              key={index} title={title} content={contentArray[index]} />
           })}
         </div>
       </div>
