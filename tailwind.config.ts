1a9b936b0340667f1a4bd5179d4256734e283788
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-11-24 01:44:25 +0100

    Mobile version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primaryblue: 'hsl(238 29% 16%)',
+        primaryred: 'hsl(14 88% 65%)',
+        bgviolet: 'hsl(273 75% 66%)',
+        bgblue: 'hsl(240 73% 65%)',
+        textverydark: 'hsl(237 12% 33%)',
+        textdark: 'hsl(240 6% 50%)',
+        divider: 'hsl(240 5% 91%)'
       },
     },
   },
